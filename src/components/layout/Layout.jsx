39b4cecc0d7afa967bcc8f8cd0e60b72b656a328
Cleef Gulfan <cleefgulfan312@outlook.com>
2023-06-22 21:15:27 +0100

    Comment explaining why the navbar was moved

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -11,6 +11,9 @@ function Layout(props) //Responsible for how the page looks, sets page out, then
       <Header userLoggedIn={props.userLoggedIn}/> {/*Calling header code from Header.jsx, passing Javascript variable through header call */}
       <div className="mainContainer">
         <SideNav /> {/*Importing Navbar from Navbar.jsx */}
+        {/*Created Side Navbar to avoid making changes to the previous Navbar made.
+        Moved to the side and changed the layout to make it closer to the initial wire frame in the
+        first meeting.*/}
         
         <main>
           {
