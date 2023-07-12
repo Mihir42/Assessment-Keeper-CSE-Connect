cf201067141e1210c9f5345a7721c35018d35565
Spiderjockey02 <benjamin.forey11@gmail.com>
2023-07-12 16:42:04 +0100

    Updated header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -6,15 +6,9 @@ export default function Header() {
 		<nav className="navbar navbar-expand-lg">
 			<div className="container">
 				<NavLink to="/" className="navbar-brand"><i className="fa fa-calendar"></i> AssessKeeper</NavLink>
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-					</ul>
-					<ul className="navbar-nav d-flex">
-						<li className="navbar-nav">
-							<a className="nav-link" href="#">Switch View</a>
-						</li>
-					</ul>
-				</div>
+				<li className="navbar-nav">
+					<a className="nav-link" href="#">Switch View</a>
+				</li>
 			</div>
 		</nav>
 	);
