fb2c2c0281ab41f938b77c94c7080c0476554925
Spiderjockey02 <benjamin.forey11@gmail.com>
2023-07-20 23:01:01 +0100

    Added search and sort on assessments

--- src/components/layout/Assessments.jsx
+++ src/components/layout/Assessments.jsx
@@ -8,22 +8,51 @@ export default function Assessments({ moduleID }) {
 	const API = new APIWrapper();
 	const [studentAssessments, setStudentAssessments] = useState(null);
 
+	// Function for updating assessment list based on search query
+	function autoComplete(e) {
+		const search = e.target.value;
+		if (search) {
+			setStudentAssessments(studentAssessments.filter(c => c.moduleName.startsWith(search)));
+		} else {
+			fetchStudentAssessments();
+		}
+	}
+
+	// Function for sorting assessment list based on option
+	async function sortChange(e) {
+		const sort = e.target.value;
+
+		switch (sort) {
+			case 'oldest': {
+				const assessments = await fetchStudentAssessments();
+				return setStudentAssessments(assessments.sort((a, b) => new Date(b.AssessmentPublishdate) - new Date(a.AssessmentPublishdate)));
+			}
+			case 'newest': {
+				const assessments = await fetchStudentAssessments();
+				return setStudentAssessments(assessments.sort((a, b) => new Date(a.AssessmentPublishdate) - new Date(b.AssessmentPublishdate)));
+			}
+			default:
+				setStudentAssessments(await fetchStudentAssessments());
+		}
+	}
+
 	// Function for fetching students assessments
-	const fetchStudentAssessments = async () => {
+	const fetchStudentAssessments = async (updateState = false) => {
 		try {
 			const response = await API.get('assessments');
 			if (response.error) throw new Error('Error');
-
-			setStudentAssessments(response);
+			if (updateState) setStudentAssessments(response);
+			return response;
 		} catch (err) {
 			console.log(err);
-			setStudentAssessments([]);
+			if (updateState) setStudentAssessments([]);
+			return [];
 		}
 	};
 
 	// Fetch student assessments on page load
 	useEffect(() => {
-		fetchStudentAssessments();
+		fetchStudentAssessments(true);
 	}, []);
 
 	// Filter based on input
@@ -36,30 +65,44 @@ export default function Assessments({ moduleID }) {
 
 	return (
 		<Card title={'Assessment'}>
-			{moduleAssessments == null ?
-				<p>No Assessments</p> :
-				<>
-					{moduleAssessments.map(task => (
-						<div className="accordion" id="accordionExample" key={task.AssessmentID}>
-							<div className="accordion-item">
-								<h2 className="accordion-header">
-									<button className="accordion-button row collapsed" type="button"
-										data-bs-toggle="collapse" data-bs-target={`#collapse_${moduleAssessments.indexOf(task)}`} aria-expanded="false" aria-controls="collapseOne">
-										<div className="col-sm-8">{task.AssessmentName}</div>
-										<div className="col-sm-3">{new Date(task.AssessmentPublishdate).toLocaleDateString('en-GB')}</div>
-									</button>
-								</h2>
-								<div id={`collapse_${moduleAssessments.indexOf(task)}`} className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-									<div className="accordion-body">
-										<p>{task.AssessmenttypeDescription}</p>
-										<p>Page URL: <a href={task.AssessmentBriefURL}>{task.AssessmentBriefURL}</a></p>
+			<>
+				<div className="row">
+					<div className="col-sm-8">
+						<input type="text" className="form-control" placeholder="Search" onChange={(e) => autoComplete(e)}/>
+					</div>
+					<div className="col-sm-3">
+						<select name="sort" id="sortSelect" className="form-control" onChange={(e) => sortChange(e)}>
+							<option value="relevant">Relevant</option>
+							<option value="oldest">Oldest</option>
+							<option value="newest">Newest</option>
+						</select>
+					</div>
+				</div>
+				{moduleAssessments == null || moduleAssessments.length == 0 ?
+					<p>No Assessments</p> :
+					<>
+						{moduleAssessments.map(task => (
+							<div className="accordion" id="accordionExample" key={task.AssessmentID}>
+								<div className="accordion-item">
+									<h2 className="accordion-header">
+										<button className="accordion-button row collapsed" type="button"
+											data-bs-toggle="collapse" data-bs-target={`#collapse_${moduleAssessments.indexOf(task)}`} aria-expanded="false" aria-controls="collapseOne">
+											<div className="col-sm-8">{task.AssessmentName}</div>
+											<div className="col-sm-3">{new Date(task.AssessmentPublishdate).toLocaleDateString('en-GB')}</div>
+										</button>
+									</h2>
+									<div id={`collapse_${moduleAssessments.indexOf(task)}`} className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+										<div className="accordion-body">
+											<p>{task.AssessmenttypeDescription}</p>
+											<p>Page URL: <a href={task.AssessmentBriefURL}>{task.AssessmentBriefURL}</a></p>
+										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-					))}
-				</>
-			}
+						))}
+					</>
+				}
+			</>
 		</Card>
 	);
 }
