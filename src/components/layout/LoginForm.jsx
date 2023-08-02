455b03b6d8c2d51d1d9428981fbaf9123542cad5
Mihir42 <mihir_mistry@outlook.com>
2023-08-02 23:12:12 +0100

    Login form can read inputs

--- src/components/layout/LoginForm.jsx
+++ src/components/layout/LoginForm.jsx
@@ -1,24 +1,79 @@
 import Card from '../UI/Card';
-import {useState} from 'react';
+import { useState } from 'react';
 import './LoginForm.scss';
-function LoginForm() {
-    // Initialisation --------------------
-    // State -----------------------------
-    const[signInAttempt, setSignInAttempt] = useState(null);
-    // Handlers --------------------------
-    // View ------------------------------
-    //42
-    return (
-        <div className="loginBox">
-            <Card title={'Login'}>
-                <label htmlFor="">User ID</label>
-                <input type="text" name="UserID" value=""/>
 
-                <label htmlFor="">Password</label>
-                <input type="text" name="Password" value=""/>
-            </Card>
-		</div> 
-    );
+const intialLogin = {
+	UserID: 0,
+	password: null,
+};
+function LoginForm() {
+	// Initialisation --------------------
+	const conformance = {
+		html2js: {
+			UserID: (value) => (value === 0 ? null : parseInt(value)),
+			password: (value) => value === '' ? null : value,
+		},
+		js2html: {
+			UserID: (value) => (value === null ? '' : value),
+			password: (value) => (value === null ? '' : value),
+		},
+	};
+
+	// State -----------------------------
+
+	const[login, setLogin] = useState(intialLogin);
+
+	// Handlers --------------------------
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setLogin({ ...login, [name]: conformance.html2js[name](value) });
+		handleLogIn();
+	};
+
+	const handleSubmit = () => {
+		console.log(`UserID=[${JSON.stringify(login)}]`);
+	};
+
+	const handleLogIn = () => {
+		if(login.UserID === 280) {
+			console.log('Yes correct');
+		}
+	};
+
+	// View ------------------------------
+
+	return (
+		<div className="loginBox">
+			<Card title={'Login'}>
+				<label className='label'>
+                    User ID
+					<input
+						type="number"
+						name="UserID"
+						value={conformance.js2html['UserID'](login.UserID)}
+						onChange={handleChange}
+					/>
+				</label>
+
+				<label>
+                    Password
+					<input
+						type="text"
+						name="password"
+						value={conformance.js2html['password'](login.password)}
+						onChange={handleChange}
+					/>
+				</label>
+
+				<button className = "loginFormSubmit"
+					type= "button"
+					onClick={handleSubmit}>
+                    Login
+				</button>
+			</Card>
+		</div>
+	);
 }
 
 export default LoginForm;
