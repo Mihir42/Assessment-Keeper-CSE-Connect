0f50b0645425a7fc48bf26c2c833e5b282476158
Mihir42 <mihir_mistry@outlook.com>
2023-08-10 22:53:07 +0100

    Login card and conformance

--- src/components/Entity/LoginCard.jsx
+++ src/components/Entity/LoginCard.jsx
@@ -1,21 +1,55 @@
 /* eslint-disable */
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import './LoginCard.scss';
 
-import './LoginCard.scss';
+const initialLoginAttempt = {
+    LoginUsername: null,
+    LoginPassword: "",
+};
+
 export default function LoginCard(props) {
     // Initialisation ------------------------------------------------
+    const conformance ={
+        html2js : {
+            LoginUsername: (value) => value === '' ? null : value,
+            LoginPassword: (value) => value === '' ? null : value,
+        },
+        js2html : {
+            LoginUsername: (value) => value === null ? '' : value,
+            LoginPassword: (value) => value === null ? '' : value,
+        }
+    };
     // State ---------------------------------------------------------
+    const [loginAttempt, setLoginAttempt] = useState(initialLoginAttempt);
     // Handlers ------------------------------------------------------
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setLoginAttempt({ ...loginAttempt, [name]: conformance.html2js[name](value) });
+    };
+    
+    const handleSubmit = () => {
+        console.log(`Module=[${JSON.stringify(loginAttempt)}]`)
+    };
     // View ----------------------------------------------------------
     return(
         <div className='loginCard'>
             <h4 className='loginCard-header'>{props.title}</h4>
             <div className='loginCard-body'>
-                {props.children}
-                <label htmlFor=''>Username</label>
-                <input type='text' name='UserName' value=''></input>
+                <label>
+                    Username
+                    <input type="text" name="LoginUsername" value={conformance.js2html["LoginUsername"](loginAttempt.LoginUsername)} onChange={handleChange } />
+                </label>
 
+                <label>
+                    Password
+                    <input type='password' name='LoginPassword' value={conformance.js2html["LoginPassword"](loginAttempt.LoginPassowrd)} onChange={handleChange} />
+                </label>
+
+                <button type='button' className='cancelButton' >Cancel</button>
+                <button type='button' className='loginButton' onClick={handleSubmit} >Login</button>
+
+            
             </div>
         </div>
     );
