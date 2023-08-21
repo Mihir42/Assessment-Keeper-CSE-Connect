999f0f20770163c6962b35958878cbf414de13ba
Mihir42 <mihir_mistry@outlook.com>
2023-08-21 22:35:04 +0100

    Assess keeper to assessment keeper

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
 	return (
 		<nav className="navbar navbar-expand-lg">
 			<div className="container">
-				<NavLink to="/modules" className="navbar-brand"><i className="fa fa-calendar"></i> AssessKeeper</NavLink>
+				<NavLink to="/modules" className="navbar-brand"><i className="fa fa-calendar"></i> Assessment Keeper</NavLink>
 				{user.UserID == undefined ?
 					<li className="navbar-nav">
 						<NavLink className="module-viewLink" href="/">Login</NavLink>
