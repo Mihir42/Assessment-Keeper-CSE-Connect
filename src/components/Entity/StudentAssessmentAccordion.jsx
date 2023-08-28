97ebaa4d73f4a4776cf52118b5a8c4c25c2d5faf
Spiderjockey02 <benjamin.forey11@gmail.com>
2023-08-28 14:03:50 +0100

    Added ability for user to add to their calender via file upload

--- src/components/Entity/StudentAssessmentAccordion.jsx
+++ src/components/Entity/StudentAssessmentAccordion.jsx
@@ -30,6 +30,38 @@ export default function StudentAssessmentAccordion({ assessment, isFavourite }) 
 		}
 	}
 
+	function downloadCalenderFile() {
+		const yyyy = `${new Date(assessment.AssessmentPublishdate).getFullYear()}`;
+		const mm = `${new Date(assessment.AssessmentPublishdate).getMonth()}`.padStart(2, '0');
+		const dd = `${new Date(assessment.AssessmentPublishdate).getDate()}`.padStart(2, '0');
+
+		const bytes = new TextEncoder().encode(`
+BEGIN:VCALENDAR
+VERSION:2.0
+BEGIN:VEVENT
+CLASS:PUBLIC
+DESCRIPTION:${assessment.AssessmentAssessmenttypeDescription}
+DTSTART;VALUE=DATE:${yyyy}${mm}${dd}
+DTEND;VALUE=DATE:${yyyy}${mm}${Number(dd) + 1}
+LOCATION:Uni
+SUMMARY;LANGUAGE=en-us:${assessment.AssessmentName}
+END:VEVENT
+END:VCALENDAR
+`);
+		const blob = new Blob([bytes], {
+			type: 'application/json;charset=utf-8',
+		});
+		// Create blob link to download
+		const url = window.URL.createObjectURL(new Blob([blob]));
+		const link = document.createElement('a');
+		link.href = url;
+		link.setAttribute('download', `${assessment.AssessmentName}.ics`);
+
+		// Add to page, click and then remove from page
+		document.body.appendChild(link);
+		link.click();
+	}
+
 	return (
 		<>
 
@@ -47,6 +79,7 @@ export default function StudentAssessmentAccordion({ assessment, isFavourite }) 
 				<>
 					<p>{assessment.AssessmentAssessmenttypeDescription}</p>
 					<p>Page URL: <a href={assessment.AssessmentBriefURL}>{assessment.AssessmentBriefURL}</a></p>
+					<button onClick={() => downloadCalenderFile()}>Download Calender file</button>
 				</>
 			</Accordion>
 		</>
